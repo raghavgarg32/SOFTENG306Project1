18f8681a15ac3261e79026a4f4c9137726b06173
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2019-07-23 16:26:13 +1200

    Add other folders

--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -1,8 +1,14 @@
 package script;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static void parse(File f){
+
     }
 }
