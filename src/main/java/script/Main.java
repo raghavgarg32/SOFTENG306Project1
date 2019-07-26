f64d79611b8a84e4a43cb2648b30ad817f94ded9
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2019-07-26 13:09:49 +1200

    Added basic file read input system

--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -1,14 +1,26 @@
 package script;
 
-import java.io.File;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        File f = new File("data/input.dot");
+        try{
+            parse(f);
+        } catch (Exception e){
+            System.out.println("bad");
+        }
+
     }
 
-    public static void parse(File f){
+    public static void parse(File f)throws FileNotFoundException, IOException {
 
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String st;
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
     }
 }
+
