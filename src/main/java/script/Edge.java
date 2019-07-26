170c85b160b4133a8629afaac82f469179ef5c09
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2019-07-26 12:13:59 +1200

    Added functionality to create edges between vertices by calling createEdge Due to this, Edge constructor is now private as edges should not be constructed outside the createEdge function

--- src/main/java/script/Edge.java
+++ src/main/java/script/Edge.java
@@ -6,9 +6,15 @@ public class Edge {
     private Vertex _toVertex;
     private int _switchCost;
 
-    public Edge(int cost, Vertex fromVertex, Vertex toVertex){
+    private Edge(int cost, Vertex fromVertex, Vertex toVertex){
         _fromVertex = fromVertex;
         _toVertex = toVertex;
         _switchCost = cost;
+    }
+
+    public static Edge createEdge(int cost, Vertex fromVertex, Vertex toVertex){
+        Edge edge = new Edge(cost, fromVertex, toVertex);
+        fromVertex.addOutgoingEdge(edge);
+        return edge;
     }
 }
