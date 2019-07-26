507a86d30c94c6bcc1b29096c1dcd61446bbce2c
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2019-07-26 12:10:01 +1200

    Added field to represent outgoing edges within vertex objects

--- src/main/java/script/Vertex.java
+++ src/main/java/script/Vertex.java
@@ -1,11 +1,18 @@
 package script;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vertex {
     private int _cost;
+    private List<Edge> _outgoingEdges;
 
     public Vertex(int cost){
         _cost = cost;
+        _outgoingEdges = new ArrayList<>();
+    }
+
+    public void addOutgoingEdge(Edge edge){
+        _outgoingEdges.add(edge);
     }
 }
