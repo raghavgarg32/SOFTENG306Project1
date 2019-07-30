7bf92bc0c603a70ae20d62ca11a7118c96105f6b
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2019-07-30 14:04:30 +1200

    Add better implementation of file parser

--- src/main/java/Graph/Main.java
+++ src/main/java/Graph/Main.java
@@ -1,11 +1,23 @@
 package Graph;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
         GraphCreator graphCreator = new GraphCreator("data/input.dot");
         Graph g = graphCreator.createGraph();
         System.out.println(g);
+
+        DotParser dp = new DotParser(new File("data/input.dot"));
+        Graph g1 = null;
+        try {
+            g1 = dp.parseGraph();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(g1);
     }
 
 
