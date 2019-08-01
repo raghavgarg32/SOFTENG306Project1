f8d42a5242570cd8ad6bb2f030a27952f671ce04
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2019-08-02 03:25:46 +1200

    Add new class Processor blcok and add more todos

--- src/main/java/ForAlgorhithms/ProcessorBlock.java
+++ src/main/java/ForAlgorhithms/ProcessorBlock.java
@@ -2,6 +2,9 @@ package ForAlgorhithms;
 
 import Graph.Vertex;
 
+/**
+ * Class to represent the time used on a processor
+ */
 public class ProcessorBlock {
     private Vertex v;
     private int startTime;
