eb856b318d2434491e2a2671a2e3256c67eacf58
Sabatr <bnguyen12152@gmail.com>
2019-08-05 15:44:15 +1200

    Adding some dummy components to test javafx functionality

--- src/main/java/visualisation/Visualiser.java
+++ src/main/java/visualisation/Visualiser.java
@@ -5,9 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import visualisation.controllers.GUIController;
 
 public class Visualiser extends Application{
 
+    private GUIController controller;
+    private static final int SCENE_HEIGHT = 800;
+    private static final int SCENE_WIDTH = 1000;
+    private int coreNum;
     /**
      * This method is called to start the visualisation
      * @param args
@@ -18,18 +23,32 @@ public class Visualiser extends Application{
 
     // Just to test if the application opens without running all of the code
     // TODO: Delete later and call startVisual instead
-    public void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 
     /**
      * Creates the GUI application
-     * @param primaryStage
+     * @param stage
      * @throws Exception
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(Visualiser.class.getResource("views/GUI.fxml"));
+        Parent root=loader.load();
+        Scene scene = new Scene(root);
 
+        //controller = loader.getController();
+
+        stage.setHeight(SCENE_HEIGHT);
+        stage.setWidth(SCENE_WIDTH);
+        stage.setResizable(false);
+        stage.setTitle("INSERT TITLE HERE");
+        stage.centerOnScreen();
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
---
+++ src/main/java/visualisation/controllers/GUIController.java
@@ -0,0 +1,18 @@
+package visualisation.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GUIController {
+
+    @FXML
+    private Button button;
+
+    /**
+     * Dummy method to test javafx XD
+     */
+    @FXML
+    private void onClick() {
+        System.out.println("clicked");
+    }
+}
