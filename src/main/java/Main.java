89aee8e40c8c4bb0ce3067dfd375c905fa796080
Magnus Rand <magnus.rand@gmail.com>
2019-08-06 18:25:09 +1200

    Add CLI functionality with support for file path and number of processors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import files.DotParser;
 import algorhithm.AStar;
 import graph.Graph;
 import files.OutputCreator;
+import org.apache.commons.cli.*;
 import scheduler.State;
 
 import java.io.File;
@@ -9,17 +10,53 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
+    private static String[] cliParser(String[] args) {
+
+        String[] result = new String[2];
+
+        Options options = new Options(); //Adding option values, e.g. -a -f -g etc. which will be parsed
+        options.addOption("f", true, "Choose input file");
+        options.addOption("p", true, "Number of processors");
+
+        CommandLineParser parser = new DefaultParser();
+        String defaultFile = "data/input.dot";
+        String defaultProcessors = "2";
+        result[0] = defaultFile;
+        result[1] = defaultProcessors;
+
+        try {
+            CommandLine cmd = parser.parse(options, args);
+
+            //This approach can be followed for Options with values
+            if(cmd.hasOption("f")) { result[0] = cmd.getOptionValue("f"); }
+            else { System.out.println("Option -f not present, default \"" + defaultFile + "\" chosen"); }
+
+            if(cmd.hasOption("p")) { result[1] =  cmd.getOptionValue("p"); }
+            else { System.out.println("Option -p not present, default \"" + defaultProcessors + "\" chosen"); }
+
+        } catch (ParseException e) { //Will be thrown of no value is provided
+            System.err.println(e);
+            System.out.println("Default values (File path: \"" + defaultFile + "\", Num. processors: \"" + defaultProcessors + "\") chosen");
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
       //  System.out.println(g);
 
-        DotParser dp = new DotParser(new File("data/input.dot"));
+        String[] result = cliParser(args);
+//        System.out.println("File path is " + result[0]);
+//        System.out.println("Processor num is " + result[1]);
+
+        DotParser dp = new DotParser(new File(result[0]));
         Graph g1 = null;
         try {
             g1 = dp.parseGraph();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        State solution = new AStar(2,g1).runAlgorhithm();
+        State solution = new AStar(Integer.parseInt(result[1]),g1).runAlgorhithm();
 
         //System.out.println(solution);
         OutputCreator out = new OutputCreator(solution);
