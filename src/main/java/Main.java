435185a0b04ee2117317eb554f7307ea5595bad3
Magnus Rand <magnus.rand@gmail.com>
2019-08-07 23:26:28 +1200

    Add option for output file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,31 +12,40 @@ public class Main {
 
     private static String[] cliParser(String[] args) {
 
-        String[] result = new String[2];
+        String[] result = new String[3];
 
         Options options = new Options(); //Adding option values, e.g. -a -f -g etc. which will be parsed
         options.addOption("f", true, "Choose input file");
         options.addOption("p", true, "Number of processors");
+        options.addOption("o", true, "Choose output file name");
 
         CommandLineParser parser = new DefaultParser();
+
+        // Default values for parser
         String defaultFile = "data/input.dot";
         String defaultProcessors = "2";
+        String defaultOutput = "output.dot";
         result[0] = defaultFile;
         result[1] = defaultProcessors;
+        result[2] = defaultOutput;
 
         try {
             CommandLine cmd = parser.parse(options, args);
 
             //This approach can be followed for Options with values
-            if(cmd.hasOption("f")) { result[0] = cmd.getOptionValue("f"); }
+            if(cmd.hasOption("f")) { result[0] = cmd.getOptionValue("f"); } // handles -f (file path) option
             else { System.out.println("Option -f not present, default \"" + defaultFile + "\" chosen"); }
 
-            if(cmd.hasOption("p")) { result[1] =  cmd.getOptionValue("p"); }
+            if(cmd.hasOption("p")) { result[1] =  cmd.getOptionValue("p"); } // handles -p (number of processors) option
             else { System.out.println("Option -p not present, default \"" + defaultProcessors + "\" chosen"); }
 
-        } catch (ParseException e) { //Will be thrown of no value is provided
+            //Add option for output file
+            if(cmd.hasOption("o")) { result[2] =  cmd.getOptionValue("o"); } // handles -o (output file name) option
+            else { System.out.println("Option -o not present, default \"" + defaultOutput + "\" chosen"); }
+
+        } catch (ParseException e) { //Will be thrown if no value is provided
             System.err.println(e);
-            System.out.println("Default values (File path: \"" + defaultFile + "\", Num. processors: \"" + defaultProcessors + "\") chosen");
+            System.out.println("Default values (File path: \"" + defaultFile + "\", Num. processors: \"" + defaultProcessors + ", Num. processors: \"" + defaultOutput + "\" chosen");
         }
 
         return result;
@@ -45,9 +54,7 @@ public class Main {
     public static void main(String[] args) {
       //  System.out.println(g);
 
-        String[] result = cliParser(args);
-//        System.out.println("File path is " + result[0]);
-//        System.out.println("Processor num is " + result[1]);
+        String[] result = cliParser(args); // result[0]: file path, result[1]: num. processors, result[2]: output
 
         DotParser dp = new DotParser(new File(result[0]));
         Graph g1 = null;
