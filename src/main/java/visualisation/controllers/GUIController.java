8a59f4034b54003c8a01c9c2f20520e4f5d30d24
Sabatr <bnguyen12152@gmail.com>
2019-08-07 20:35:23 +1200

    Beginning of process

--- src/main/java/visualisation/controllers/GUIController.java
+++ src/main/java/visualisation/controllers/GUIController.java
@@ -3,6 +3,8 @@ package visualisation.controllers;
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 import org.graphstream.graph.Graph;
 import org.graphstream.ui.swingViewer.ViewPanel;
@@ -23,6 +25,7 @@ public class GUIController {
     @FXML
     private void initialize() {
        createInputGraphVisual();
+       createProcessVisual();
     }
 
     /**
@@ -36,10 +39,11 @@ public class GUIController {
         viewer.enableAutoLayout();
         // Creates the panel on a section of the application
         ViewPanel view = viewer.addDefaultView(false);
-        graphPane.setPrefSize(600,600);
-        view.setPreferredSize(new Dimension(600,600));
+        graphPane.setPrefSize(500,500);
+        view.setPreferredSize(new Dimension(500,500));
         SwingNode node = new SwingNode();
         node.setContent(view);
+
         graphPane.getChildren().add(node);
     }
 
@@ -51,9 +55,15 @@ public class GUIController {
          * The basis:
          *  Create a table - Columns depend on the number of processors
          *  Rows will be added dynamically as more tasks are iterated through OR possibly just set the maximum possible as the rows
-         *  Change colours and stuff
+         *  Change colours when a task is on that processor
+         *  How am I gonna get the number of processors? A listener maybe?
          *  Watch out for large inputs. Might screw over some layout.
          */
+        processPane.setPrefSize(500,500);
+        TableView table = new TableView();
+        table.setPrefSize(500,500);
+        table.getColumns().add(new TableColumn("Test"));
+        processPane.getChildren().add(table);
     }
 
 
