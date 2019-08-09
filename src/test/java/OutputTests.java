45af169a239c418bced7a0508ec91c3418b952a1
Chuyang Chen <40534565+saddboys@users.noreply.github.com>
2019-08-09 12:27:27 +1200

    Added outputTest which does not work yet

--- src/test/java/OutputTests.java
+++ src/test/java/OutputTests.java
@@ -1,7 +1,31 @@
+import files.DotParser;
+import files.OutputCreator;
+import graph.Graph;
+import jdk.javadoc.internal.doclets.toolkit.util.DocFinder;
 import org.junit.Test;
+import scheduler.State;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class OutputTests {
 
+
+    /**
+     * This method is helps test different input files.
+     * @param inputURL
+     */
+    private Graph createGraph(String inputURL) {
+
+        DotParser dp = new DotParser(new File("data/" + inputURL));
+        Graph g1 = null;
+        try {
+            g1 = dp.parseGraph();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return g1;
+    }
 
     /**
      * Tests if the output file is correct.
@@ -10,6 +34,12 @@ public class OutputTests {
      */
     @Test
     public void testOutputFile() {
+        Graph g = createGraph("input3.dot");
+        State s = new State(1, g);
+        OutputCreator oc = new OutputCreator(s);
+        oc.createOutputFile("output3.dot");
 
+        //Graph g1 = createGraph("output3.dot");
+        //System.out.println(g1);
     }
 }
